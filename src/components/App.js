a6c0f265670b769876c37b3645b3a7954d744458
Gzim <gzimasanni@gmail.com>
2021-04-16 23:58:30 +0200

    Add components

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
 import './App.css';
+import Display from './Display';
+import ButtonPanel from './ButtonPanel'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Welcome
-        </p>
-      </header>
+      < Display />
+      < ButtonPanel/>
     </div>
   );
 }
