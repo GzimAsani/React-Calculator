d0215264ebf6b4be5fbfd1cd206505114f5d38f0
Gzim <gzimasanni@gmail.com>
2021-04-22 01:24:49 +0200

    Add operate logic

---
+++ src/logic/operate.js
@@ -0,0 +1,35 @@
+import Big from 'big.js';
+
+function operate(numA, numB, operation) {
+
+  const a = Big(numA);
+  const b = Big(numB);
+
+  let value;
+
+  if(operation === '+') {
+    value = a.plus(b);
+  }
+
+  if(operation === '-') {
+    value = a.minus(b);
+  }
+
+  if(operation === 'X') {
+    value = a.times(b);
+  }
+
+  if(operation === '÷') {
+    value = a.div(b);
+  }
+
+  if(operation === '%') {
+    value = a.mod(b);
+  }
+
+  return value.toString();
+
+
+}
+
+export default operate
