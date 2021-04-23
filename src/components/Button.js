6528317b3269c5341d32f1cfd49a33c0bd9151ce
Gzim <gzimasanni@gmail.com>
2021-04-23 15:22:42 +0200

    Update button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,19 +1,32 @@
 import PropTypes from 'prop-types';
 
-function Button({ btnName }) {
+const Button = ({ btnName, clickHandler }) => {
+  const handleClick = () => clickHandler(btnName);
+  const fetchResult = (btnName) => {
+    switch (btnName) {
+      case '0':
+        return 'Button zero';
+      case '+':
+      case '-':
+      case 'x':
+      case '/':
+      case '=':
+        return 'Button operator';
+      default:
+        return 'Button';
+    }
+  };
   return (
-    <button type="button">
-      { btnName }
+    <button type="button" onClick={handleClick} classbtnName={fetchResult(btnName)}>
+      {btnName}
     </button>
-  );
-}
 
-Button.propTypes = {
-  btnName: PropTypes.string,
+  );
 };
 
-Button.defaultProps = {
-  btnName: '',
+Button.propTypes = {
+  btnName: PropTypes.string.isRequired,
+  clickHandler: PropTypes.func.isRequired,
 };
 
 export default Button;
