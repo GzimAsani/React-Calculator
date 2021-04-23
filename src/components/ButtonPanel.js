68328c1f0a1d116874fa76288d566f9328a656b3
Gzim <gzimasanni@gmail.com>
2021-04-24 01:54:05 +0200

    Update button panel according to the review

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -3,36 +3,37 @@ import PropTypes from 'prop-types';
 import Button from './Button';
 
 function ButtonPanel({ clickHandler }) {
+  const handleClick = (buttonName) => clickHandler(buttonName);
   return (
     <div className="main-div">
       <div className="group-1">
         <Button clickHandler={clickHandler} name="AC" />
-        <Button name="+/-" clickHandler={clickHandler} />
-        <Button name="%" clickHandler={clickHandler} />
-        <Button name="÷" clickHandler={clickHandler} />
+        <Button name="+/-" clickHandler={handleClick} />
+        <Button name="%" clickHandler={handleClick} />
+        <Button name="÷" clickHandler={handleClick} />
       </div>
       <div className="group-2">
-        <Button name="7" clickHandler={clickHandler} />
-        <Button name="8" clickHandler={clickHandler} />
-        <Button name="9" clickHandler={clickHandler} />
-        <Button name="x" clickHandler={clickHandler} />
+        <Button name="7" clickHandler={handleClick} />
+        <Button name="8" clickHandler={handleClick} />
+        <Button name="9" clickHandler={handleClick} />
+        <Button name="x" clickHandler={handleClick} />
       </div>
       <div className="group-3">
-        <Button name="4" clickHandler={clickHandler} />
-        <Button name="5" clickHandler={clickHandler} />
-        <Button name="6" clickHandler={clickHandler} />
-        <Button name="-" clickHandler={clickHandler} />
+        <Button name="4" clickHandler={handleClick} />
+        <Button name="5" clickHandler={handleClick} />
+        <Button name="6" clickHandler={handleClick} />
+        <Button name="-" clickHandler={handleClick} />
       </div>
       <div className="group-4">
-        <Button name="1" clickHandler={clickHandler} />
-        <Button name="2" clickHandler={clickHandler} />
-        <Button name="3" clickHandler={clickHandler} />
-        <Button name="+" clickHandler={clickHandler} />
+        <Button name="1" clickHandler={handleClick} />
+        <Button name="2" clickHandler={handleClick} />
+        <Button name="3" clickHandler={handleClick} />
+        <Button name="+" clickHandler={handleClick} />
       </div>
       <div className="group-5">
-        <Button name="0" clickHandler={clickHandler} />
-        <Button name="." clickHandler={clickHandler} />
-        <Button name="=" clickHandler={clickHandler} />
+        <Button name="0" clickHandler={handleClick} />
+        <Button name="." clickHandler={handleClick} />
+        <Button name="=" clickHandler={handleClick} />
       </div>
     </div>
   );
